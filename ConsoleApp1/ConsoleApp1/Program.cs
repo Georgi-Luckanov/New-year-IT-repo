11ffd270201a762438a9208fd91ea5ade3296c2c
Georgi-Luckanov <et.georgi.l@eduiteh.eu>
2026-01-05 11:17:14 +0200

    nova zadacha 2
    nova zadacha 2

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string Name = "Georgi Luckanov";
+float hight = 1.88f;
+int year = 2008;
+string gender = "male";
+string Hobby = "Fortnite";
+
+Console.WriteLine( Name);
+Console.WriteLine( hight);
+Console.WriteLine( year);
+Console.WriteLine( gender);
+Console.WriteLine( Hobby);
