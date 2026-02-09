02b7d514d08077f36f8245280b37839b40f2ac80
Georgi-Luckanov <et.georgi.l@eduiteh.eu>
2026-02-09 09:49:09 +0200

    zadacha
    zadacha

---
+++ Purva zadacha ot vtori srok/Purva zadacha ot vtori srok/Program.cs
@@ -0,0 +1,22 @@
+﻿/*for (int i = 0; i <= 100; i++)
+{
+    Console.Write(i + " ");
+
+    if (i % 10 == 9) 
+    {
+        Console.WriteLine();
+    }
+}
+Console.WriteLine();*/
+
+int num = 0;
+while (num <= 100)
+{
+    Console.Write(num + " ");
+    if(num % 10 == 9)
+    {
+        Console.WriteLine();
+    }
+    num++;
+}
+Console.WriteLine();
---
+++ vtora zadacha ot vtori srok/vtora zadacha ot vtori srok/Program.cs
@@ -0,0 +1,48 @@
+﻿//zadacha s "for"
+for (int i = 0; i <= 100; i++)
+{
+    Console.Write(i + " ");
+
+    if (i % 10 == 9) 
+    {
+        Console.WriteLine();
+    }
+}
+Console.WriteLine();
+int num = 0;
+
+//zadacha s "do while"
+while (num <= 100)
+{
+    Console.Write(num + " ");
+    if (num % 10 == 9)
+    {
+        Console.WriteLine();
+    }
+    num++;
+}
+Console.WriteLine();
+do
+{
+    Console.WriteLine(num + " ");
+    if (num % 10 == 9)
+    { 
+        Console.WriteLine(); 
+    }
+    num++;
+} while (num <= 100);
+Console.WriteLine();
+
+//Task:
+//Input N numbers and output the one with the highest value 
+int numbrer = int.Parse(Console.ReadLine());
+int max = int.MinValue;
+for (int i = 0; i < numbrer; i++)
+{
+    int number = int.Parse(Console.ReadLine());
+        if (number >  max)
+        {
+        max = number;
+        }
+}
+Console.WriteLine(max);
